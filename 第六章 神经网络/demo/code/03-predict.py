cf0320302636f02b1df95ee2730100d8d36aba64
duan <dwangchn@qq.com>
2020-10-29 16:47:46 +0800

    update

--- 第六章 神经网络/demo/code/03-predict.py
+++ 第六章 神经网络/demo/code/03-predict.py
@@ -29,31 +29,3 @@ new_reg_data.to_excel(outputfile)  # 结果输出
 print('预测结果为：\n',new_reg_data.loc[2014:2015,:])  # 预测结果展示
 
 
-
-# 代码6-6
-
-import matplotlib.pyplot as plt
-from sklearn.svm import LinearSVR
-
-inputfile = 'D:/教育/python_machine_learning/第六章 神经网络/test/tmp/new_reg_data_GM11.xls'  # 灰色预测后保存的路径
-data = pd.read_excel(inputfile)  # 读取数据
-feature = ['x1', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x13']  # 属性所在列
-data_train = data.loc[range(1994,2014)].copy()  # 取2014年前的数据建模
-data_mean = data_train.mean()
-data_std = data_train.std()
-data_train = (data_train - data_mean)/data_std  # 数据标准化
-x_train = data_train[feature].as_matrix()  # 属性数据
-y_train = data_train['y'].as_matrix()  # 标签数据
-
-linearsvr = LinearSVR()  # 调用LinearSVR()函数
-linearsvr.fit(x_train,y_train)
-x = ((data[feature] - data_mean[feature])/data_std[feature]).as_matrix()  # 预测，并还原结果。
-data[u'y_pred'] = linearsvr.predict(x) * data_std['y'] + data_mean['y']
-outputfile = 'D:/教育/python_machine_learning/第六章 神经网络/test/tmp/new_reg_data_GM11_revenue.xls'  # SVR预测后保存的结果
-data.to_excel(outputfile)
-
-print('真实值与预测值分别为：\n',data[['y','y_pred']])
-
-fig = data[['y','y_pred']].plot(subplots = True, style=['b-o','r-*'])  # 画出预测结果图
-plt.show()
-
